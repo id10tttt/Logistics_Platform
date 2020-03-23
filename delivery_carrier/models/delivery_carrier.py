c1a1c45870144f914fb08601c7f5eca91fe20ce9
1di0t <dgqcjx@gmail.com>
2020-03-23 15:48:27 +0800

    fix

--- delivery_carrier/models/delivery_carrier.py
+++ delivery_carrier/models/delivery_carrier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from odoo import models, api, fields
+from odoo import models, api, fields, _
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -12,3 +12,12 @@ class DeliveryCarrier(models.Model):
 
     delivery_type = fields.Selection(selection_add=[('network', "Network")])
     network_id = fields.Many2one('route.network', 'Network')
+
+    # 网络计算方式的价格
+    def network_rate_shipment(self, order):
+        return {
+            'success': True,
+            'price': 0.0,
+            'error_message': False,
+            'warning_message': False
+        }
