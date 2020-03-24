600c83cc05c21767c1c3aa5ee84b961c13f972c5
1di0t <dgqcjx@gmail.com>
2020-03-24 14:46:06 +0800

    remove field

--- delivery_carrier/models/delivery_carrier.py
+++ delivery_carrier/models/delivery_carrier.py
@@ -11,7 +11,7 @@ class DeliveryCarrier(models.Model):
     _inherit = 'delivery.carrier'
 
     delivery_type = fields.Selection(selection_add=[('network', "Network")])
-    network_id = fields.Many2one('route.network', 'Network')
+    # network_id = fields.Many2one('route.network', 'Network')
 
     # 网络计算方式的价格
     def network_rate_shipment(self, order):
